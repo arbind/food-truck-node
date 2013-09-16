7a0e8f81adaeb35ea2b7bbcb7c817ddf9af155cc
Arbind Thakur and Rob Pak <dev+arbind+rpak@carbonfive.com>
2013-09-15 22:16:06 -0700

    correct javscript in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('coffee-script')
-_ = require('underscore')
-global.localEnvironment = 'development'
-module.exports = require('./app/server-app')
+require('coffee-script');
+global._ = require('underscore');
+global.localEnvironment = 'development';
+module.exports = require('./app/server-app');
